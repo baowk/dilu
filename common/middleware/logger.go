060669e6331f59a5067da8696354a17b5f352345
baowk <baowk1024@gmail.com>
2023-09-14 21:22:31 +0800

    请求日志处理

--- common/middleware/logger.go
+++ common/middleware/logger.go
@@ -3,7 +3,7 @@ package middleware
 import (
 	"bufio"
 	"bytes"
-	"fmt"
+	"encoding/json"
 	"io"
 	"net/http"
 	"strings"
@@ -18,7 +18,6 @@ import (
 // LogLayout 日志layout
 type LogLayout struct {
 	//Metadata  map[string]interface{} // 存储自定义原数据
-	//Time      time.Time
 	Method    string //方法
 	Path      string // 访问路径
 	Query     string // 携带query
@@ -28,75 +27,7 @@ type LogLayout struct {
 	Error     string // 错误
 	Cost      string // 花费时间
 	Source    string // 来源
-	//Result    string        //返回数据
 }
-
-// type Logger struct {
-// 	// Filter 用户自定义过滤
-// 	Filter func(c *gin.Context) bool
-// 	// FilterKeyword 关键字过滤(key)
-// 	FilterKeyword func(layout *LogLayout) bool
-// 	// AuthProcess 鉴权处理
-// 	AuthProcess func(c *gin.Context, layout *LogLayout)
-// 	// 日志处理
-// 	Print func(LogLayout)
-// 	// Source 服务唯一标识
-// 	Source string
-// }
-
-// func (l Logger) SetLoggerMiddleware() gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		start := time.Now()
-// 		path := c.Request.URL.Path
-// 		query := c.Request.URL.RawQuery
-// 		var body []byte
-// 		if l.Filter != nil && !l.Filter(c) {
-// 			fmt.Println("aaaa")
-// 			body, _ = c.GetRawData()
-// 			// 将原body塞回去
-// 			c.Request.Body = io.NopCloser(bytes.NewBuffer(body))
-// 		}
-// 		c.Next()
-// 		cost := time.Since(start)
-// 		layout := LogLayout{
-// 			//Time:      time.Now(),
-// 			Path:      path,
-// 			Query:     query,
-// 			IP:        c.ClientIP(),
-// 			UserAgent: c.Request.UserAgent(),
-// 			Error:     strings.TrimRight(c.Errors.ByType(gin.ErrorTypePrivate).String(), "\n"),
-// 			Cost:      cost.String(),
-// 			Source:    l.Source,
-// 		}
-// 		if l.Filter != nil && !l.Filter(c) {
-// 			layout.Body = string(body)
-// 		}
-// 		if l.AuthProcess != nil {
-// 			// 处理鉴权需要的信息
-// 			l.AuthProcess(c, &layout)
-// 		}
-// 		if l.FilterKeyword != nil {
-// 			// 自行判断key/value 脱敏等
-// 			l.FilterKeyword(&layout)
-// 		}
-// 		// 自行处理日志
-// 		l.Print(layout)
-// 	}
-// }
-
-// func DefaultLogger() gin.HandlerFunc {
-// 	return Logger{
-// 		Filter: func(c *gin.Context) bool {
-// 			return true
-// 		},
-// 		Print: func(layout LogLayout) {
-// 			// 标准输出,k8s做收集
-// 			v, _ := json.Marshal(layout)
-// 			fmt.Println(string(v))
-// 		},
-// 		//Source: "",
-// 	}.SetLoggerMiddleware()
-// }
 
 // LoggerToFile 日志记录到文件
 func LoggerToFile() gin.HandlerFunc {
@@ -120,46 +51,33 @@ func LoggerToFile() gin.HandlerFunc {
 		}
 
 		c.Next()
-		uri := c.Request.RequestURI
-		// 结束时间
-		if c.Request.Method == http.MethodOptions {
-			return
-		}
 
-		// rt, bl := c.Get("result")
-		// var result = ""
-		// if bl {
-		// 	rb, err := json.Marshal(rt)
-		// 	if err != nil {
-		// 		core.Log.Warn("json Marshal result error", zap.Error(err))
-		// 	} else {
-		// 		result = string(rb)
-		// 	}
-		// }
-		cost := time.Since(startTime)
+		go writeLog(startTime, body, c)
+	}
+}
 
-		fmt.Println(cost.Nanoseconds())
+func writeLog(startTime time.Time, body string, c *gin.Context) {
+	// 结束时间
+	if c.Request.Method == http.MethodOptions {
+		return
+	}
+	cost := time.Since(startTime)
+	layout := LogLayout{
+		Path:      c.Request.RequestURI,
+		Query:     c.Request.URL.RawQuery,
+		IP:        ips.GetIP(c),
+		UserAgent: c.Request.UserAgent(),
+		Error:     strings.TrimRight(c.Errors.ByType(gin.ErrorTypePrivate).String(), "\n"),
+		Cost:      cost.String(),
+		Body:      body,
+		Method:    c.Request.Method,
+		Source:    core.Cfg.Server.Name,
+	}
 
-		layout := LogLayout{
-			Path:      uri,
-			Query:     c.Request.URL.RawQuery,
-			IP:        ips.GetIP(c),
-			UserAgent: c.Request.UserAgent(),
-			Error:     strings.TrimRight(c.Errors.ByType(gin.ErrorTypePrivate).String(), "\n"),
-			Cost:      cost.String(),
-			Body:      body,
-			Method:    c.Request.Method,
-			Source:    core.Cfg.Server.Name,
-		}
-
-		if cost.Milliseconds() < 200 {
-			core.Log.Info("Req", zap.Any("", layout))
-		} else {
-			core.Log.Warn("Req", zap.Any("", layout))
-		}
-
-		// if c.Request.Method != "OPTIONS" && config.LoggerConfig.EnabledDB && statusCode != 404 {
-		// 	SetDBOperLog(c, clientIP, statusCode, reqUri, reqMethod, latencyTime, body, result, statusBus)
-		// }
+	b, _ := json.Marshal(layout)
+	if cost.Milliseconds() < 200 {
+		core.Log.Info(string(b))
+	} else {
+		core.Log.Warn(string(b))
 	}
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.20
 require (
 	github.com/aliyun/aliyun-oss-go-sdk v2.2.8+incompatible
 	github.com/aws/aws-sdk-go v1.44.319
-	github.com/baowk/dilu-core v0.0.0-20230914025130-871e663e423c
+	github.com/baowk/dilu-core v0.0.0-20230914130018-de96e7ff9fff
 	github.com/buger/jsonparser v1.1.1
 	github.com/fsnotify/fsnotify v1.6.0
 	github.com/gin-gonic/gin v1.9.1
